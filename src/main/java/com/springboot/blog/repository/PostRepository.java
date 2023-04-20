b7c050f5d75af0a203726961d1b3de4598fe9928
Huy.th <nhokpio95@gmail.com>
2023-04-21 06:26:33 +0700

    Create PostController

--- src/main/java/com/springboot/blog/repository/PostRepository.java
+++ src/main/java/com/springboot/blog/repository/PostRepository.java
@@ -2,6 +2,9 @@ package com.springboot.blog.repository;
 
 import com.springboot.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
 }
